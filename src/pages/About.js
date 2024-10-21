c76aea72cf788b30594916371b5cb0e4872086b2
Alberto Rivera <albertorivera@Albertos-MacBook-Air.local>
2024-10-20 18:32:42 -0700

    better about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,72 +1,66 @@
-// AboutMe.js
 import React from 'react';
 import Navbar from "../components/Navbar";
-
-
+import opensauce from "../imgs/opensauce2024.png";
 
 const About = () => {
   return (
-
     <div className="min-h-screen min-w-screen bg-[#003366] relative overflow-hidden">
-      
-    {/* Background Grid */}
-    <div className="grid-background"></div>
+      {/* Background Grid */}
+      <div className="grid-background z-0"></div>
 
-    <Navbar></Navbar> 
+      <Navbar />
 
-    <div class="bg-black text-white py-8 px-4 md:px-8 lg:px-16 about-me">
-      <h1 class="text-3xl font-semibold mb-4">About Me</h1>
+      <div className="bg-black ml-[10%] mr-[10%] text-white py-8 px-4 md:px-8 lg:px-16 about-me">
+        
+        {/* Image and Text Container */}
+        <div className="flex justify-between">
+          
+          {/* Text on the left */}
+          <div className="w-1/2">
+            <h1 className="text-6xl font-semibold">Alberto Rivera</h1>
+            <h2 className="text-xl text-gray-400">AKA: Alberto G. Rivera, Beto, Capt Beto</h2>
 
-      <p class="mb-4">
-        Hello! 
-        Thank for for checking out my website! How did you even find this? Anyway...
-        <p>
-        If you're here, you already know my name, but just to satisfy our SEO (Search Engine Optimization) gods, let me reiterate.
-        </p>
-        <p>
-        I'm Alberto Rivera, starup founder, software engineer, sailor, and all around swell guy.
-        </p>
-      </p>
+            <p className="mt-8 mb-4">
+              Hello!
+              <p>
+              Thank for for checking out my website!
+              </p>
+              <br></br>
+              <p>
+              I'm a software engineer with too many interests but mostly focusing on computer vision and deep learning.
+              </p>
+              <br></br>
+              <p>
+              I'm currently Co-Founder and CTO of Hellometer. A Y-Combinator (S20) backed startup applying computer vision to retail spaces. Previously I founded Strada Labs, with the mission of providing the most accurate urban traffic data to transit agencies. Before that I was a research engineer at Ford Motor Company studying where computer vision and artificial intelligence could be applied throughout the whole company.
+              </p>
+              <br></br>
+              <p>
+              Outside of my day job, I'm also often thinkering on other project, usually involving computer vision, and increasingly, robotics. Outside of thinkering I'm also a competitive sailor on the J/88, J/22 and Laser classes. 
+              </p>
+            </p>
 
-      <h2 class="text-xl font-semibold mb-2">Professional Background</h2>
+            <p className="mb-4">
+              Let's Connect
+              <br></br>
+              Feel free to connect with me on <a href="https://www.linkedin.com/in/albertogrivera/" className="text-blue-400">LinkedIn</a>.
+            </p>
 
-      <h3 class="text-l font-semibold mb-2">Hellometer</h3>
-      <p class="mb-4">
-        As the CTO at Hellometer, I lead the Computer Vision efforts and guide the direction of the rest of our technology stack.
-      </p>
+            <p>Happy coding!</p>
+          </div>
 
-      <h3 class="text-l font-semibold mb-2">Strada Labs</h3>
-      <p class="mb-4">
-        This was my previous startup, which also harnessed the power of computer vision to gather urban traffic data.
-      </p>
-
-      <h3 class="text-l font-semibold mb-2">Ford Motor Company</h3>
-      <p class="mb-4">
-        I was a Research Engineer focusing on applying the latest artificial inteligence technologies around the entire company.
-      </p>
-
-      <h2 class="text-xl font-semibold mb-2">Expertise</h2>
-      <ul class="list-disc ml-6 mb-4">
-        <li>Computer Vision: Leveraging advanced computer vision techniques to extract meaningful insights from visual data.</li>
-        <li>Deep Learning: Applying state-of-the-art deep learning models to solve complex problems and drive business innovation.</li>
-      </ul>
-
-      <h2 class="text-xl font-semibold mb-2">Beyond Technology</h2>
-      <p class="mb-4">
-        While my professional life revolves around technology, I'm not just a tech enthusiast—I'm also an avid sailor. 
-        I find joy and excitement in competitive sailing, strategy, and adaptability are key. 
-      </p>
-
-      <p class="mb-4">
-        Let's Connect
-        <br></br>
-        Feel free to connect with me on <a href="https://www.linkedin.com/in/albertogrivera/" class="text-blue-400">LinkedIn</a>.
-      </p>
-
-      <p>Happy coding!</p>
-    </div>
+        {/* Image on the right */}
+  <div className="ml-4 w-1/2 flex justify-center">
+    <img
+      src={opensauce}
+      className="h-auto w-auto object-contain max-w-full max-h-full z-20"
+      alt="Alberto Rivera with 3D printed robot arm at OpenSauce 2024"
+    />
   </div>
 
+          
+        </div>
+      </div>
+    </div>
   );
 };
 
